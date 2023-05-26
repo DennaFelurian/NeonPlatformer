a5bd2073a3162f60551cd0a77a9df8b0c816f5ad
Denise <Denise@DESKTOP-DVJ9UL2.fritz.box>
2023-05-26 19:50:45 +0200

    updated main

--- src/com/realtutsgml/neon/window/Game.java
+++ src/com/realtutsgml/neon/window/Game.java
@@ -1,6 +1,9 @@
 package com.realtutsgml.neon.window;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
 	
@@ -22,9 +25,57 @@ public class Game extends Canvas implements Runnable {
 
 	public void run()
 	{
-		System.out.println("Thread has begun!");
+		long lastTime = System.nanoTime();
+		double amountOfTicks= 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int updates = 0;
+		int frames = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				tick();
+				updates++;
+				delta--;
+			}
+			render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS:" +frames + " Ticks;"  + updates);
+				frames = 0;
+				updates = 0;
+			}
+		}
+		
 	}
 	
+	private void tick() {
+		
+	}
+	
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		//////////////////////////////////
+		
+		//Draw Here///////////////////////
+		g.setColor(Color.black);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		//////////////////////////////////
+		g.dispose();
+		bs.show();
+		
+	}
 	public static void main(String args []){
 		
 		new Window (800, 600, "Neon Platform Game Prototype", new Game());
