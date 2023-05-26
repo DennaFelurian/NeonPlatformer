ec4a5c716a06bafcd44ab53c59309aafdb7bddde
Denise <Denise@DESKTOP-DVJ9UL2.fritz.box>
2023-05-26 19:21:33 +0200

    Created main

---
+++ src/com/realtutsgml/neon/window/Game.java
@@ -0,0 +1,34 @@
+package com.realtutsgml.neon.window;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
+	
+
+	private static final long serialVersionUID = -6261436164361361187L;
+	
+	private boolean running = false;
+	private Thread thread;
+	
+	public synchronized void start() {
+		if (running)
+			return;
+		
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+		
+	}
+
+	public void run()
+	{
+		System.out.println("Thread has begun!");
+	}
+	
+	public static void main(String args []){
+		
+		new Window (800, 600, "Neon Platform Game Prototype", new Game());
+	}
+	}
+
+
